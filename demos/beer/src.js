095b33411596f4d878933d20163a48bcbfed1a8a
ManueleAsbtract <manuele.cozzolino@asbtract.it>
2016-12-12 12:22:33 +0100

    Created Bottle with AMD syntax

--- demos/beer/src.js
+++ demos/beer/src.js
@@ -1,42 +1,27 @@
 dojo.provide("demos.beer.src");
-//Each javascript source file must have at least one dojo.provide() call at the TOP of the file,
-//corresponding to the file name,  before any calls to dojo.require() are made.
 
-//dojo.provide() tells the loader that the module has been provided for the given name.
-//It also creates a JavaScript object for the name.
+require([
+  "dojox/rpc/Service",
+  "demos/beer/src/Bottle",
+  "demos/beer/src/Lady",
+  "demos/beer/src/dnd",
+  "dojo/domReady!"
+], (Service, Bottle, Lady, dnd) => {
+  var ladyRef = dojo.byId('lady');
+  var lady = new beer.Lady();
+  ladyRef.appendChild(lady.domNode);
 
-dojo.require("dojox.rpc.Service");
-//La root del path passato alle funzioni del loader (provide, require), è la cartella in cui è
-//contenuta la cartella di dojo, in questo caso dojoDemos. Infatti si sta chiamando la funzione
-//require dell'oggetto dojo
+  var shelf = dojo.byId("shelf-offer").firstChild;
+  for(var i = 0; i < 10; i++){
+    var bottle = new beer.Bottle({
+      beerIndex: i,
+      size:"large"
+    });
+    shelf.appendChild(bottle.domNode);
+  }
 
-dojo.require("demos.beer.src.Bottle");
-dojo.require("demos.beer.src.Lady")
-dojo.require("demos.beer.src.dnd");
-
-dojo.addOnLoad(function(){
-// dojo.addOnLoad() has been deprecated in favor of the (equivalent) dojo.ready function,
-//and more recently, in favor of the The AMD API, and the domReady! plugin
-
-//	beer.api = new dojox.rpc.Service(dojo.moduleUrl("demos.beer.resources","api.smd"));
-
-	var shelf = dojo.byId("shelf-offer").firstChild;
- 	//dojo.byId("shelf-offer") restituisce un domNode (equivalente a getElementById)
-
-	for(var i = 0; i < 10; i++){
-		var bottle = new beer.Bottle({
-			beerIndex: i,
-			size:"large"
-		});
-		shelf.appendChild(bottle.domNode);
-	}
-
-	dojo.query("div", shelf)
-		.forEach(function(n){
-			new beer.MadeDnd(n);
-		});
-	//dojo.query returns a list of DOM nodes based on a CSS selector.
-	//The first argument is the selector which is a CSS selector string that identifies
-	//the nodes that need to be retrieved. The second argument is an optional context
-	//which limits the scope of the selector and only children of the will be considered.
+  dojo.query("div", shelf)
+    .forEach(function(n){
+      new beer.MadeDnd(n);
+    });
 });
--- demos/beer/src/Bottle.js
+++ demos/beer/src/Bottle.js
@@ -1,45 +1,57 @@
-dojo.provide("demos.beer.src.Bottle");
+// Defining modules is accomplished using the define function.
+// An important thing to note when defining modules is that the callback
+// function is only ever invoked once—the returned value is cached by the loader.
 
-dojo.require("dijit._Widget");
+// You will see the first parameter included in some places in Dojo for backwards-compatibility
+// for pre-AMD syntax.
 
-dojo.declare("beer.Bottle", dijit._Widget, {
-	// summary:
-	//		A Beer visual.
-	
-	beerIndex: 0,
-	
-	size: "small",
-	sizes: {
-		small: 18, large: 36
-	},
-	
-	postCreate: function(){
-		
-		dojo.addClass(this.domNode, "dijitInline beerBox-" + this.size);
-		var offset = this.sizes[this.size];
-		dojo.style(this.domNode,{
-			backgroundPosition:"-" + (this.beerIndex * offset) + "px 1px"
-		});
+define(
+	"demos.beer.src.Bottle",
+	[
+		"dojo/_base/declare",
+		"dijit/_Widget"
+	],
+	(declare, _Widget) => {
+		declare("beer.Bottle", _Widget, {
+			// summary:
+			//		A Beer visual.
+
+			beerIndex: 0,
+
+			size: "small",
+			sizes: {
+				small: 18, large: 36
+			},
+
+			postCreate: function(){
+
+				dojo.addClass(this.domNode, "dijitInline beerBox-" + this.size);
+				var offset = this.sizes[this.size];
+				dojo.style(this.domNode,{
+					backgroundPosition:"-" + (this.beerIndex * offset) + "px 1px"
+				});
+			}
+
+		})
+
+		declare("beer.UserProfile", _Widget, {
+			// summary:
+			//		A shelf of beers for a particular Person
+
+			user:"",
+
+			setUser: function(username){
+				this.user = username || null;
+				if(this.user){
+					dojo.xhrGet({
+						url: "getBeers.php",
+						content: { user: this.user },
+						load: dojo.hitch(this,function(data){
+
+						})
+					});
+				}
+			}
+		})
 	}
-
-});
-
-dojo.declare("beer.UserProfile", dijit._Widget, {
-	// summary:
-	//		A shelf of beers for a particular Person
-	
-	user:"",
-
-	setUser: function(username){
-		this.user = username || null;
-		if(this.user){
-			dojo.xhrGet({
-				url: "getBeers.php",
-				content: { user: this.user },
-				load: dojo.hitch(this,function(data){
-					
-				})
-			});
-		}
-	}
-});
+)
